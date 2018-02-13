242929c8291ac0fd1554f8cc219d803170b284cf
이끼룩 <saram4030@gmail.com>
2018-02-13 15:44:38 +0900

    [FIX] ComponentScan 애노테이션 제거
    ComponentScan은 SpringBootApplication 안에서 자동으로 추가됨

--- src/main/java/com/woowahan/woowahan2018/Woowahan2018Application.java
+++ src/main/java/com/woowahan/woowahan2018/Woowahan2018Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
 public class Woowahan2018Application {
 
 	public static void main(String[] args) {
