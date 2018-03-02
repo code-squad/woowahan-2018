06aec8f8f64e90da9e4d141741be911c1b7af1f1
이끼룩 <saram4030@gmail.com>
2018-03-02 11:16:52 +0900

    [FIX] withBasicAuth를 이용한 테스트 코드 복구

--- src/test/java/com/woowahan/woowahan2018/controller/BoardAcceptanceTest.java
+++ src/test/java/com/woowahan/woowahan2018/controller/BoardAcceptanceTest.java
@@ -2,14 +2,18 @@ package com.woowahan.woowahan2018.controller;
 
 import com.woowahan.woowahan2018.dto.BoardDto;
 import com.woowahan.woowahan2018.dto.CommonResponse;
+import com.woowahan.woowahan2018.dto.ResponseStatus;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import support.AcceptanceTest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -19,35 +23,41 @@ public class BoardAcceptanceTest extends AcceptanceTest {
 	@Test
 	public void create() throws Exception {
 		BoardDto newBoard = new BoardDto("Test Board");
-		CommonResponse response = template().postForObject("/api/boards/", newBoard, CommonResponse.class);
-		assertThat(response, is(CommonResponse.success("Board를 성공적으로 생성했습니다.")));
+		CommonResponse response = basicAuthTemplate().postForObject("/api/boards/", newBoard, CommonResponse.class);
+
+		assertThat(response.getStatus(), is(ResponseStatus.OK));
+		assertThat(response.getMessage(), is("BOARD.CREATE"));
 	}
 
 	@Test
-	public void create_null() throws Exception{
+	public void create_null() throws Exception {
 		BoardDto newBoard = new BoardDto();
-		CommonResponse response = template().postForObject("/api/boards/", newBoard, CommonResponse.class);
-		assertThat(response, is(CommonResponse.error("이름을 입력하세요.")));
+		List<CommonResponse> responses = Arrays.asList(basicAuthTemplate().postForObject("/api/boards/", newBoard, CommonResponse[].class));
+
+		assertThat(responses.size(), is(1));
+		assertThat(responses.get(0).getStatus(), is(ResponseStatus.FAIL));
+		assertThat(responses.get(0).getMessage(), is("TEXT.EMPTY"));
 	}
 
 	@Test
-	public void create_blank() throws Exception{
+	public void create_blank() throws Exception {
 		BoardDto newBoard = new BoardDto("");
-		CommonResponse response = template().postForObject("/api/boards/", newBoard, CommonResponse.class);
-		assertThat(response, is(CommonResponse.error("이름을 입력하세요.")));
+		List<CommonResponse> responses = Arrays.asList(basicAuthTemplate().postForObject("/api/boards/", newBoard, CommonResponse[].class));
+
+		assertThat(responses.size(), is(1));
+		assertThat(responses.get(0).getStatus(), is(ResponseStatus.FAIL));
+		assertThat(responses.get(0).getMessage(), is("TEXT.EMPTY"));
 	}
 
 	@Test
 	public void showAll() {
-        BoardDto newBoard = new BoardDto("Test Board!!");
-        template().postForObject("/api/boards/", newBoard, CommonResponse.class);
+		BoardDto newBoard = new BoardDto("Test Board!!");
+		basicAuthTemplate().postForObject("/api/boards/", newBoard, CommonResponse.class);
 
-		CommonResponse response = template().getForObject("/api/boards/", CommonResponse.class);
-		log.debug("response : {} ", response);
-		LinkedHashMap<String, List<LinkedHashMap<String, String>>> content
-				= (LinkedHashMap<String, List<LinkedHashMap<String, String>>>) response.getContent();
-		List<LinkedHashMap<String, String>> boards = content.get("boards");
-		assertThat(boards.get(1).containsValue("Test Board!!"), is(true));
+		CommonResponse response = basicAuthTemplate().getForObject("/api/boards/", CommonResponse.class);
+
+		log.debug("response: {}", response);
+		assertThat(response.getContent().toString(), containsString(newBoard.getName()));
 	}
 
 }
